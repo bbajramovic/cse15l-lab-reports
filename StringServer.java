08d1cf6a43e4fa1e572247b105f1b21c875c6078
bbajramovic <146884553+bbajramovic@users.noreply.github.com>
2023-10-18 23:31:35 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -4,19 +4,38 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-class StringHandler {
-  int length = args.length; //error
-  int num = 0;
-  int args[] = new int[100]
-  public String handleRequest(URI url){
-    if (url.getPath().equalds("/")){
-      return String.format("?d", num); //printing the digit before each string output
-      num += 1;
-    }
-    if (url.getPath().equals("/add-message")){
-      String[] parameters = url.getQuery().split("=");
-      args.add("%s\n"); //????
-       
-      for(int i = 0; i , length; length += 1){
-        System.out.print(args[i]);
+class StringHandler implements URLHandler {
+  List<String> lines;
+  String path;
+  StringHandler(String path){
+    this.path = path;
+    this.lines = Files.readAllLines(Paths.get(path));
+  }
+  public String handleRequest(URI url) {
+    String query = url.getQuery();
+    if(url.getPath().equals("/add-message")) {
+      if(query.startsWith("s=")) {
+        String toAdd = query.split("=")[1];
+        this.lines.add(toAdd);
+        System.out.println(lines);
       }
+    }
+  }
+}
+class StringServer {
+  public static void main(String[] args) throws IOException {
+    if(args.length == 0){
+      System.out.println("Missing port number! Try any number between 1024 to 49151");
+      return;
+    }
+    if(args.length == 1){
+      System.out.println("Missing file path! Give a path to a text file as the second argument.");
+      return;
+    }
+
+    int port = Integer.parseInt(args[0]);
+
+    Server.start(port, new StringHandler(args[1]));
+  }
+}
+      
