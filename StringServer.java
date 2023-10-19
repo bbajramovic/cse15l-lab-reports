28e3b766688f36dec9ba7b5dc0ebae1e08797ad9
bbajramovic <146884553+bbajramovic@users.noreply.github.com>
2023-10-19 13:00:44 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -1,24 +1,37 @@
 import java.io.IOException;
 import java.net.URI;
-import java.nio.file.Files;
-import java.nio.file.Paths;
-import java.util.List;
 
-class Handler{
-  in num = 0;
-  int newArg[] = new int[100]
-  public String handleRequest(URI url) {
-    if (url.getPath().equals("/")){
-      return String.format(%d", num); //printing the digit before each string output
-      num += 1;
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+ String[] array = null;
+  int count = 1;
+
+    public String handleRequest(URI url) {
+            if (url.getPath().contains("/add-message")) {
+                String[] parameters = url.getQuery().split("=");
+              String str = String.format(count) + "."+ parameters[1] \n"
+                count++ 
+                array.add(str); //add this to array
+                return String.format("%s ", array); 
+                }
+            }
+            return "404 Not Found!";
+        }
     }
-    if (url.getPath().equals("/add-message")){
-      String[] parameters = url.getQuery().split("=");
-      //how to add string parameters into args
-      for(int i = 0; i < args.length; length +=1) { //printing out contents of each string in array
-        System.out.println(args[i]);
-      }
+}
+
+class NumberServer {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
     }
-  }
 }
       
