d4ccda59b375ef3ca45656c173269a887355e0ac
bbajramovic <146884553+bbajramovic@users.noreply.github.com>
2023-10-18 21:13:01 -0700

    Create StringServer.java

---
+++ StringServer.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.net.URI;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+class StringHandler implements URLHandler {
+  int length = args.length;
+  int num = 0;
+  List<String> args;
+  String path;
+  public String handleRequest(URI url) {
+    if (url.getPath().equals("/")){
+      return String.format("%d", num); //printing the digit before each string output
+      num += 1;
+      if (url.getPath().equals("/add-message")){
+        if(query.startsWith("s=")) {
+          String[] parameters = url.getQuery().split("=")[n];
+          String path = String.join("\n", args) + "\n";
+          Files.write(Paths.get(this.path), path.getBytes());
+          for(int i = 1; i < length; length += 1) {
+          System.out.println(args[i]);
+          }
+        }
+      }
+    }
+      
+  
