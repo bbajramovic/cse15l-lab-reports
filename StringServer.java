e7779b1251a757849b6d36237ac2ea3f7240065e
bbajramovic <146884553+bbajramovic@users.noreply.github.com>
2023-10-18 23:34:38 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -4,7 +4,7 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-class StringHandler implements URLHandler {
+class StringHandler {
   List<String> lines;
   String path;
   StringHandler(String path){
@@ -20,22 +20,6 @@ class StringHandler implements URLHandler {
         System.out.println(lines);
       }
     }
-  }
-}
-class StringServer {
-  public static void main(String[] args) throws IOException {
-    if(args.length == 0){
-      System.out.println("Missing port number! Try any number between 1024 to 49151");
-      return;
-    }
-    if(args.length == 1){
-      System.out.println("Missing file path! Give a path to a text file as the second argument.");
-      return;
-    }
-
-    int port = Integer.parseInt(args[0]);
-
-    Server.start(port, new StringHandler(args[1]));
   }
 }
       
