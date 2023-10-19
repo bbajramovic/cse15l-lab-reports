f307585902ae0c74584963ed4107f89c14a64793
bbajramovic <146884553+bbajramovic@users.noreply.github.com>
2023-10-18 23:44:22 -0700

    Update StringServer.java

--- StringServer.java
+++ StringServer.java
@@ -4,20 +4,19 @@ import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.List;
 
-class StringHandler {
-  List<String> lines;
-  String path;
-  StringHandler(String path){
-    this.path = path;
-    this.lines = Files.readAllLines(Paths.get(path));
-  }
+class Handler{
+  in num = 0;
+  int newArg[] = new int[100]
   public String handleRequest(URI url) {
-    String query = url.getQuery();
-    if(url.getPath().equals("/add-message")) {
-      if(query.startsWith("s=")) {
-        String toAdd = query.split("=")[1];
-        this.lines.add(toAdd);
-        System.out.println(lines);
+    if (url.getPath().equals("/")){
+      return String.format(%d", num); //printing the digit before each string output
+      num += 1;
+    }
+    if (url.getPath().equals("/add-message")){
+      String[] parameters = url.getQuery().split("=");
+      //how to add string parameters into args
+      for(int i = 0; i < args.length; length +=1) { //printing out contents of each string in array
+        System.out.println(args[i]);
       }
     }
   }
